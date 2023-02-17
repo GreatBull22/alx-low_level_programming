b6e66f7940a625973ffb18ec8f03704b19dcb05f
GreatBull22 <brandonmpofu22@gmail.com>
2023-02-16 23:49:39 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,13 @@
-/** main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19"
- * followed by a newline, to standard error.
- * Return: Always 1
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry
+ * Return:1
  */
 
 int main(void)
-
-	write(2 "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
-	return(1);
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
